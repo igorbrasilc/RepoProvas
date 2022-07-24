e15c0d690064ec0c1ef4abdefa2969561b94e17b
Igor C. Brasil <igorbrasilc@hotmail.com>
2022-07-24 16:55:21 -0300

    adds thunderclient tests

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,12 @@ import AppLog from "./utils/AppLog";
 import app from "./app";
 import "dotenv/config";
 
-const { PORT, HOST } = process.env;
+// const { PORT, HOST } = process.env;
 
-const port = +PORT || 4000;
-const host = HOST || "localhost";
+// const port = +PORT || 4000;
+// const host = HOST || "localhost";
 
 app.get("/", async (req: Request, res: Response) => res.send("API online"));
-app.listen(port, host, () =>
-  AppLog("Server", `Server running on port ${port}`)
+app.listen(+process.env.PORT || 4000, () =>
+  AppLog("Server", `Server running on port ${+process.env.PORT || 4000}`)
 );
